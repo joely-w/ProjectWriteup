8c62bf3b0c04ad6051d1e59fc30bfcb03fa167ae
Joel Winterton <joel.winterton@outlook.com>
2022-11-21 22:33:45 +0000

    Started fixing SIR model

--- Gillespie.R
+++ Gillespie.R
@@ -3,7 +3,6 @@ sim <- function(x, params){
   Y <- x[3]
   Z <- x[4]
   N <- X+Y+Z
-  
   beta <- params['beta']
   gamma <- params['gamma']
   
@@ -14,36 +13,35 @@ sim <- function(x, params){
     transmission=c(-1,1,0),
     recovery=c(0,-1,1)
   )
+  
   rate_total <- sum(rates)
   if(rate_total==0)
     tau <- Inf
   else
     tau <- rexp(n=1,rate=rate_total)
-  event <- sample.int(n=2,size=1,prob=rates/rate_total)
   
+  event <- sample.int(n=2,size=1,prob=rates/rate_total)
   x+c(tau,transitions[[event]])
 }
 
-simulate <- function (x, params, maxstep = 10000) {
+simulate <- function (x, params, maxstep = 1500) {
   output <- array(dim=c(maxstep+1,4))
   colnames(output) <- names(x)
   output[1,] <- x
   k <- 1
-  ## loop until either k > maxstep or
-  ## there are no more infectives
   while ((k <= maxstep) && (x["Y"] > 0)) {
     k <- k+1
     output[k,] <- x <- sim(x,params)
   }
   as.data.frame(output[1:k,])
 }
-set.seed(56856583)
 nsims <- 10
-xstart <- c(time=0,X=392,Y=8,Z=0) #initial conditions
-params <- c(mu=0.02,beta=60,gamma=365/13) #parameters
+xstart <- c(time=0,X=760,Y=3,Z=0)
+params <- c(beta=0.450626519034621,gamma=0.00221583886792478)
 
 require(plyr)
 simdat <- rdply(
   nsims,
   simulate(xstart,params)
 )
+d_ply(simdat,".n",function(x)lines(Y~time,data=x,col=.n))
