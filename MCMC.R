be84c7dce288ffc78efd259219ff3e800d81d357
Joel Winterton <joel.winterton@outlook.com>
2023-02-21 21:33:42 +0000

    Added correlation plots
    Actually this time

---
+++ MCMC.R
@@ -0,0 +1,13 @@
+
+# Install packages 
+install.packages('MASS');
+install.packages('deSolve');
+install.packages('latex2exp');
+install.packages('tidyverse');
+
+# Load libraries 
+library(MASS)
+library(deSolve)
+library(latex2exp)
+library(tidyverse)
+dev.new(width=8, height=3.5)
